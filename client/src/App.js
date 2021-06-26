b52295d62060d69be24338b97ab6817dd87d6c24
Angela <angelacr@ymail.com>
2021-06-26 13:56:18 -0500

    adding jubomtron and navbar to html, adding more components, and adding files in routes folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/navbar';
+import Jumbotron from './Components/Jumbotron/jumbotron';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <Jumbotron/>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ client/src/Components/Jumbotron/jumbotron.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./style.css";
+
+function Jumbotron()
+{
+    return(
+    <div class="jumbotron">
+    <div class="container">
+        <h1>jumbotron Holder</h1>
+    </div>
+</div>
+)
+}
+
+export default Jumbotron
